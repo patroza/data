3cf7b428a315b1b3a512cc03d04515865d6bccc0
Patrick Roza <contact@patrickroza.com>
2023-02-22 17:49:48 +0100

    expand

--- test/Equal2.ts
+++ test/Equal2.ts
@@ -1,4 +1,7 @@
-import { makeEqualClass } from "@effect/data/test/Equal1"
+import { EqualTest1, makeEqualClass } from "@effect/data/test/Equal1"
+
+// fine
+export class EqualTest3 extends EqualTest1 {}
 
 // bad
 // 'extends' clause of exported class 'EqualTest2' has or is using private name 'Equal'
